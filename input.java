cd82b344c7f75a91eaab45f4f285d5979b8d4199
ankitaborade02 <ankitaborade64@gmail.com>
2024-12-05 15:34:26 +0530

    input

---
+++ input.java
@@ -0,0 +1,14 @@
+
+// take two variable a and b and print their sum
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);    // for taking input from user
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);         // printing output
+    }
+}
